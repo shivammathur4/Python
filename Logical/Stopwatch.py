8fe632e76e3cabd7f2c713d9ea89801bfe4e9874
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-30 10:17:17 +0530

    Add: Stopwatch Code

---
+++ Logical/Stopwatch.py
@@ -0,0 +1,38 @@
+import time
+timeList=[]
+def SecondToTimeFormat(second):
+    '''
+    Description:
+        Convert second to Time format HH:MM:SS
+    Parameter:
+        seconds
+    Return:
+        List of seconds,minutes,hours
+    '''
+    try:
+        minutes,second=second//60,second%60
+        hours,minutes=minutes//60,minutes%60
+        return [second,minutes,hours]
+    except Exception as ex:
+        print(ex)
+
+def TimeElapsed():
+    '''
+    Description:
+        Calculates time elapsed between start and stop for stop watch
+    Parameters:
+        No Parameters
+    Return:
+        Difference between start and stop
+    '''
+    try:
+        input("Press Enter To Start Stop Watch ")
+        startTime=time.time()
+        input("Press Enter To Stop Stop Watch  ")
+        stopTime=time.time()
+        return (stopTime-startTime)
+    except Exception as ex:
+        print(ex)
+
+timeList=SecondToTimeFormat(TimeElapsed())
+print("Time Elapsed {0}::{1}::{2} ".format(timeList[2],timeList[1],timeList[0]))
