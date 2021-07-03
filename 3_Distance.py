3faddcb2a750f086e3e45852a10d92436d43e700
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-07-03 10:01:33 +0530

    Reffactored the Code

--- 3_Distance.py
+++ 3_Distance.py
@@ -5,52 +5,9 @@
 @Last Modified time:7:00 PM
 @Title: Euclidian Distance Between origin and Cartesian Co-oridnates  
 '''
-
-import re
 import math
-
-
-def euclidianDistance(x,y):
-    """
-    Description:
-        Method calculates distance between origin and caretsian
-        cordinates.
-    Parameters:
-        Takes two parameters x(co-ordiante) and y(ordinate).
-    Return:
-        Euclidian distance between origin and given cartesian
-        point
-    """
-    try:
-        return math.sqrt(math.pow(x,2)+math.pow(y,2))
-    except Exception as ex:
-        print(ex)
-
-    #Validation Method
-def validation(value):
-    '''
-    Description:
-        Method validates values given as parameters.
-    Parameters:
-        Takes value as parameter.
-    Return:
-        value after validating.
-    '''
-    try:
-        regexFloat="^[+-]?([0-9]*[.])?[0-9]+$"
-        if(re.match(regexFloat,value)):
-            return float(value)
-        print("Invalid Input")
-        quit()
-    except Exception as ex:
-        print(ex)
-try:  
-    inputX_Coordinate=(input("Enter X-coordinate\n"))
-    inputX_Coordinate=validation(inputX_Coordinate)
-    inputY_Ordinate=(input("Enter Y-coordinate\n"))
-    inputY_Ordinate=validation(inputY_Ordinate)
-    #Function Call
-    print(euclidianDistance(inputX_Coordinate,inputY_Ordinate))
-except Exception as ex:
-    print(ex)
+x,y = [int(x) for x in input("Enter X and Y coordinates ").split()]
+#calculating Euclidean Distance
+distanceBetweenPoints = math.sqrt(math.pow(x,2) + math.pow(y,2))
+print("The distance of the points from the origin is ",round(distanceBetweenPoints,2)) 
 
