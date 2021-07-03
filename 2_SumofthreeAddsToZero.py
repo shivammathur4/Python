c15a48960615419219e7e2a037c28206ad31e81f
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-07-03 10:13:35 +0530

    Refactored the Code

--- 2_SumofthreeAddsToZero.py
+++ 2_SumofthreeAddsToZero.py
@@ -5,68 +5,30 @@
 @Last Modified time: 6:00 PM
 @Title: Sum of three integers add to zero
 '''
+Description:	    
+        Run three nested loops with loop counter i, j, k and check  if the sum of elements at i’th, j’th, k’th is equal to zero or not.
+'''           
+       
 
+def findTriplets(arr, n):
+ 
+    found = False
+    for i in range(0, n-2):
+     
+        for j in range(i+1, n-1):
+         
+            for k in range(j+1, n):
+             
+                if (arr[i] + arr[j] + arr[k] == 0):
+                    print(arr[i], arr[j], arr[k])
+                    found = True
+     
+             
+    
+    if (found == False):
+        print(" not exist ")
+ 
 
-def inputFromUser():
-    '''
-    Description:
-        Takes integer as input for array of integers and validate
-        user input should not be less than 1
-    Parameters:
-        No parameters
-    Return:
-        If returns a list of Integers
-    '''
-    try:
-        listOfIntegers=[]
-        numOfintegers=int(input("Enter number of integers to add\n"))
-        #Validation
-        if(numOfintegers<0):
-            print("invalid input only whole number allowed")
-            quit()
-        for count in range(numOfintegers):
-            value=int(input("Enter integers\n"))
-            listOfIntegers.append(value)
-        return listOfIntegers
-    except Exception as ex:
-        print(ex)
-def findThreeIntegersAddToZero(list):
-    '''
-    Description:
-        Takes list of Integers and find triplet of integers which
-        adds to zero.
-    Parameters:
-        list of integers 
-    Return:
-        This function returns flag value
-    '''
-    try:
-        for firstPlace in range(len(list)-2):
-            for secondPlace in range(firstPlace+1,(len(list)-1)):
-                for thirdPlace in range(secondPlace+1,len(list)):
-                    if((list[firstPlace]+list[secondPlace]+list[thirdPlace])==0):
-                        print(list[firstPlace],list[secondPlace],list[thirdPlace])
-        return 1
-    except Exception as ex:
-        print(ex)
- #Implemented Main definition     
-def main():
-    '''
-    Description:
-        It gives entry point to program. If is used to call other definition
-        in program code.
-    Parameters:
-        Do not take any parameters.
-    Return:
-        No return type.
-    '''
-    try:
-        flag=0
-        numberList=inputFromUser()
-        flag=findThreeIntegersAddToZero(numberList)
-        if(flag==0):
-            print("No three integers add to zero")
-    except Exception as ex:
-        print(ex)
-if __name__=="__main__":
-    main()
+arr = [0, -1, 2, -3, 1]
+n = len(arr)
+findTriplets(arr, n)
