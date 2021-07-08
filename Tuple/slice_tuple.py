6cac6dcbdaea360988091dab15343979daae478e
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-07-08 18:16:50 +0530

    [Shivam] Refactor : Code through Slice method

--- Tuple/slice_tuple.py
+++ Tuple/slice_tuple.py
@@ -6,19 +6,17 @@
 '''
 
 from loggers import logger
+tuple1 = (1, 2, 3, 4, 5, 6)
 def slice_tuple():
         '''
         Slice a tuple
         '''
-        try:
-            mytuple = tuple(int(item) for item in input("Enter the integer elements(seprated by space): ").split())
-            start = int(input('Enter the start index where you want to start for slicing :'))
-            stop = int(input('Enter the last index where you want to stop for slicing :'))
-            sliceList = []    # store the tuple element after slicing
-            for index in range(start,stop):       # For tuple slicing also use myTuple[start:stop:step]
-                sliceList.append(mytuple[index])
-            return tuple(sliceList)
-        except Exception as ex:
-            logger.error(ex)
+try:
+    slice_this = tuple1[3:5]
+    print(slice_this)
+except Exception as ex:
+    logger.error(ex)
+    
+    
 
-print(slice_tuple())           
+slice_tuple()          
