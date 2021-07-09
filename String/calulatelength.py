3ecb55d94186298cf42db8955f6967849718b168
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-07-09 11:09:36 +0530

    [Shivam] Add : Length of String Program

---
+++ String/calulatelength.py
@@ -0,0 +1,19 @@
+'''
+@Author: Shivam Mathur
+@Date: 2021-07-08
+@Last Modified by: Ranjith G C
+@Title : Program Aim to check the length of a string
+'''
+from logger import logger
+
+def length():
+    '''
+    Description:
+        This function finds the length of a string.
+    '''
+
+    try:
+        str = "Shivam"
+        return(len(str))
+    except Exception as ex:
+        logger.error(ex)    
